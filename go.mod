cf827003995e698a5aa2fa2dd4b55ce94be2c449
khhalil <anasalhomseee@gmail.com>
2023-04-05 17:15:33 +0200

    old

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github/khalelalsam/funtemps
+module funtemps
 
-go 1.20
+go 1.19
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github/khalelalsam/funtemps/conv"
+	"funtemps/conv"
 	"math"
 )
 
@@ -28,17 +28,11 @@ func init() {
 
 	// Definerer og initialiserer flagg-variablene
 	flag.Float64Var(&fahr, "F", 0.0, "temperatur i grader fahrenheit")
-
 	flag.Float64Var(&cels, "C", 0.0, "temperatur i grader Celsius")
 	flag.Float64Var(&kel, "K", 0.0, "temperatur i grader kelvin")
 	flag.StringVar(&temp, "t", "C", "temperatur i sun")
-
-	// Du må selv definere flag-variablene for "C" og "K"
 	flag.StringVar(&out, "out", "C", "beregne temperatur i C - celsius, F - farhenheit, K- Kelvin")
-
 	flag.StringVar(&funfacts, "funfacts", "sun", "\"fun-facts\" om sun - Solen, luna - Månen og terra - Jorden")
-	// Du må selv definere flag-variabelen for -t flagget, som bestemmer
-	// hvilken temperaturskala skal brukes når funfacts skal vises
 
 }
 
@@ -90,25 +84,18 @@ func main() {
 			fmt.Printf("%.2f °F er %.2f °C.", fahr, celsius)
 		}
 
-		//fmt.Printf("%.2f degrees Fahrenheit is equal to %.2f degrees Celsius.",fahr, conv.FahrenheitToCelsius(fahr))
-
-		// Kalle opp funksjonen FahrenheitToCelsius(fahr), som da
-		// skal returnere °C
-		//fmt.Println("0°F er -17.78°C")
 	} else if out == "C" && isFlagPassed("K") {
 
-		C := conv.KelvinToCelsius(kel)
+		Celsius := conv.KelvinToCelsius(kel)
 
-		if C == math.Trunc(C) {
+		if Celsius == math.Trunc(Celsius) {
 
-			CInt := int(C)
+			Int := int(Celsius)
 
-			fmt.Printf("%.2f  °K er %.d °C.", kel, CInt)
+			fmt.Printf("%.2f  °K er %.d °C.", kel, Int)
 		} else {
-			fmt.Printf("%.2f °K er %.2f °C.", kel, C)
+			fmt.Printf("%9.2f °K er %9.2f °C.", kel, Celsius)
 		}
-
-		//fmt.Printf("%.2f degrees kelvin is equal to %.2f degrees celsius.",kel,conv.KelvinToCelsius(kel))
 
 	} else if out == "F" && isFlagPassed("C") {
 
@@ -122,68 +109,61 @@ func main() {
 			fmt.Printf("%.2f °C er %.2f °F.", cels, f)
 		}
 
-		//fmt.Printf("%.2f degrees Celsius is equal to %2f degrees Fahrenheit.",cels,conv.CelsiusToFahrenheit(cels))
-
 	} else if out == "F" && isFlagPassed("K") {
 
-		f := conv.KelvinToFahrenheit(kel)
+		Fahrenheit := conv.KelvinToFahrenheit(kel)
 
-		if f == math.Trunc(f) {
+		if Fahrenheit == math.Trunc(Fahrenheit) {
 
-			farhenheitInt := int(f)
-			fmt.Printf("%.2f °K er %.d °F.", kel, farhenheitInt)
+			Int := int(Fahrenheit)
+			fmt.Printf("%9.2f °K er %.d °F.", kel, Int)
 		} else {
-			fmt.Printf("%.2f °K er %.2f °F.", kel, f)
+			fmt.Printf("%9.2f °K er %9.2f °F.", kel, Fahrenheit)
 		}
 
 		//fmt.Printf("%.2f degrees kelvin is equal to %.2f degrees Farhrenheit.",kel,conv.KelvinToFahrenheit(kel))
 
 	} else if out == "K" && isFlagPassed("C") {
 
-		k := conv.CelsiusToKelvin(cels)
+		Kelvin := conv.CelsiusToKelvin(cels)
 
-		if k == math.Trunc(k) {
+		if Kelvin == math.Trunc(Kelvin) {
 
-			KInt := int(k)
+			Int := int(Kelvin)
 
-			fmt.Printf("%.2f °C er %.d °K.", cels, KInt)
+			fmt.Printf("%9.2f °C er %.d °K.", cels, Int)
 		} else {
-			fmt.Printf("%.2f °C er %.2f °K.", cels, k)
+			fmt.Printf("%9.2f °C er %9.2f °K.", cels, Kelvin)
 		}
-
-		//fmt.Printf("%.2f degrees Celsius is equal to %.2f degrees kevlin.",cels ,conv.CelsiusToKelvin(cels))
 
 	} else if out == "K" && isFlagPassed("F") {
 
-		kK := conv.FahrenheitToKevlin(fahr)
-		if kK == math.Trunc(kK) {
+		Kelvin := conv.FahrenheitToKevlin(fahr)
 
-			KInt := int(kK)
+		if Kelvin == math.Trunc(Kelvin) {
 
-			fmt.Printf("%.2f °F er %.d °K.", fahr, KInt)
+			Int := int(Kelvin)
+
+			fmt.Printf("%9.2f °F er %.d °K.", fahr, Int)
 		} else {
-			fmt.Printf("%.2f °F er %.2f °K.", fahr, kK)
+			fmt.Printf("%9.2f °F er %9.2f °K.", fahr, Kelvin)
 		}
-
-		//fmt.Printf("%.2f degrees Farhenheit is equal to %.2f degrees Kelvin.",fahr,conv.FahrenheitToKevlin(fahr))
-
 	}
 
 	if temp == "C" && isFlagPassed("funfacts") && funfacts != "terra" && funfacts != "luna" {
 
-		fmt.Print("Temperatur på ytre lag av Solen 5506.85°C.\n")
-		fmt.Print("Temperatur i Solens kjerne er 15 000 000°C.")
+		fmt.Printf("Temperatur på ytre lag av Solen 5506.85°C.\n")
+		fmt.Printf("Temperatur i Solens kjerne er 15 000 000°C.")
 
 	} else if funfacts == "luna" && isFlagPassed("funfacts") {
 
-		fmt.Print("Temperatur på Månens overflate om natten er lik -183\n")
-		fmt.Print("Temperatur på Månens overflate om dagen er lik 106 \n")
+		fmt.Printf("Temperatur på Månens overflate om natten er lik -183\n")
+		fmt.Printf("Temperatur på Månens overflate om dagen er lik 106 ")
 
 	} else if funfacts == "terra" && isFlagPassed("funfacts") {
 
-		fmt.Print("Laveste temperatur målt på Jordens overflate -89.4\n")
-		fmt.Print("Temperatur i Jordens indre kjerne 9392 \n")
-
+		fmt.Printf("Laveste temperatur målt på Jordens overflate -89.4\n")
+		fmt.Printf("Temperatur i Jordens indre kjerne 9392 ")
 	}
 }
 
@@ -199,13 +179,3 @@ func isFlagPassed(name string) bool {
 	})
 	return found
 }
-
-/*
-func formatWithCommas(str string) string {
-	n := len(str)
-	if n <= 3 {
-		return str
-	}
-	return formatWithCommas(str[:n-3]) + "," + str[n-3:]
-}
-*/
